8e387cc6f508e4932812da40358ab73c8af1e1a5
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-05-27 19:27:54 +0700

    Create navbar

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 </head>
 
 <body>
+    <?php require('./menu.php'); ?>
 </body>
 
 </html>
---
+++ menu.php
@@ -0,0 +1,19 @@
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand"><i class="fa fa-code" aria-hidden="true"></i></a>
+    <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="my-nav" class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Sản Phẩm <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+    <form class="form-inline" method="post" action="search.php">
+        <input class="form-control mr-3" type="text" name="searchtxt" placeholder="Search...">
+        <input class="btn btn-success" type="submit" name="search" value="Search">
+    </form>
+</nav>
+</div>

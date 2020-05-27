28b68001ce89809dcfb7315d148aa8e2964061d7
Ta Long Khanh <longkhanh02092000@gmail.com>
2020-05-27 19:30:07 +0700

    Create footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <div class="footer">
+        <p>&copy;Bài Tập Lớn Nhóm 13</p>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -10,10 +10,19 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <style>
+        .footer {
+            height: 50px;
+            line-height: 50px;
+            background: #ccc;
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
     <?php require('./menu.php'); ?>
+    <?php require('./footer.php'); ?>
 </body>
 
 </html>
